8906cd073c7eb4e7d4d7ed212cd8493269dfc600
KLisabeth <professional.translations4ppl@gmail.com>
2020-03-11 19:49:21 +0100

    II.write-test

--- II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -1,9 +1,15 @@
-test("remove vowels from all words in array", function() {
-  // Arrange
-  // Act
-  // Assert
-});
+var removeVowels = require("./remove-vowels-in-array");
 
+test("remove vowels from all words in array", function() {
+  var input = ["Irina", "Etza", "Daniel"];
+  // Arrange
+  var output = removeVowels(input);
+  // Act
+  var result = ["__i_a", "___a", "_a_ie_"];
+  // Assert
+  expect(output).toEqual(result);
+});
 // example
+
 // input: ["Irina", "Etza", "Daniel"]
 // expected output: ["rn", "tz", "Dnl"]
--- II.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ II.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,19 @@
+var evenNumbers = require("./get-even-numbers");
+
+test("Get even number", function() {
+    var input = [22, 13, 73, 82, 4];
+    var output = evenNumbers(input);
+    
+    expect(output).toEqual(output);
+});
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
+var evenNumbers = require("./get-even-numbers");
+
+test("Get even number", function() {
+    var input = [22, 13, 73, 82, 4];
+    var output = evenNumbers(input);
+    
+    expect(output).toEqual(output);
+});
